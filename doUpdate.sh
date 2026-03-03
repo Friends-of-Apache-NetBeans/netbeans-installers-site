8831f77246918662c530eb744cce6854dfc7d93f
Pieter van den Hombergh (homberghp) <pieter.van.den.hombergh@gmail.com>
2026-03-03 16:18:54 +0100

    get more infor from build.properties

--- doUpdate.sh
+++ doUpdate.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
-release_tag=$1
+# repo typically stays constant
+repo=Friends-of-Apache-NetBeans/netbeans-installers
+# If release_tag argument is not given, use latest according to github
+if [[ $# -lt 1 ]]; then 
+    release_tag=$(gh release list -R ${repo} --json tagName | jq '.[] .tagName' | sed -e's/"//g' | head -1)
+else 
+    release_tag=$1
+fi
+
+echo "using release tag ${release_tag}"
+echo "using repo tag ${repo}"
+# cleanup
 rm -rf dist
 mkdir -p dist
-gh release download --clobber ${release_tag} -R Friends-of-Apache-NetBeans/netbeans-installers -D dist -p build.properties
-gh release view -R Friends-of-Apache-NetBeans/netbeans-installers ${release_tag} --json assets |\
-    jq  '.assets[] | .name +";"+.url+";"+.digest' | sed -e 's/"//g' | grep -v yaml | grep -v properties > dist/assets.txt
+# get the build propertes
+gh release download --clobber ${release_tag} -R ${repo} -D dist -p build.properties
+# get the assest data using the API
+gh api -H "'Accept: application/vnd.github+json'" -H "'X-GitHub-Api-Version: 2022-11-28'" \
+   /repos/${repo}/releases/tags/${release_tag} | jq '.assets[] | .name+";"+.browser_download_url+";"+.digest+";"+(.download_count|tostring)' \
+   | sed -e 's/"//g' | grep -v yaml | grep -v properties > dist/assets.txt
 
 nb_release=$(cat dist/build.properties | grep netbeans.version | cut -d= -f2)
-jdkversion=$(cat dist/build.properties | grep jdk.version | cut -d= -f2)
 
 if [[ ${nb_release} =~ .*rc*. ]]; then
     echo release candidate ${nb_release}
